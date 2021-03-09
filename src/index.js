69e3d7d8f351184b0a0691b36f9dec68e9adfeb7
David Gunner <gunnerjnr@live.co.uk>
2021-03-09 08:36:29 +0000

    2. create a component that renders JSX

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// components
+function RenderJSX ()
+{
+  return (
+    <div className='book'>
+      <div className='title'>
+        The Title
+      </div>
+      <div className='author'>
+        The Author
+      </div>
+      <ul className='stats'>
+        <li className='rating'>
+          5 stars
+        </li>
+        <li className='isbn'>
+          12-345678-910
+        </li>
+      </ul>
+    </div>
+  );
+}
+
 function JSXExercises ()
 {
   return (
-    <div>JSX Exercises</div>
-  )
+    <>
+      <div>JSX Exercises</div>
+      <hr />
+      <br />
+      <RenderJSX />
+    </>
+  );
 }
 
 ReactDOM.render(
   <JSXExercises />,
-  document.querySelector('#root')
+  document.querySelector( '#root' )
 );
