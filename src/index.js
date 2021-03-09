f70865573721e5ff84bf53439d7a12da4ef480c8
David Gunner <gunnerjnr@live.co.uk>
2021-03-09 08:28:01 +0000

    1. replace contents simila to hello world lesson

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function JSXExercises ()
+{
+  return (
+    <div>JSX Exercises</div>
+  )
+}
 
+ReactDOM.render(
+  <JSXExercises />,
+  document.querySelector('#root')
+);
