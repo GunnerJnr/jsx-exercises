4800a7d48915a0a774652efbda27b48b5239f6a5
David Gunner <gunnerjnr@live.co.uk>
2021-03-11 21:57:30 +0000

    Exercise 6 solution - see comments for more info

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 
 // components
 
@@ -15,6 +16,7 @@ function JSXExercises ()
       <TheLongWay />
       <ValidIndicator />
       <Greeting />
+      <Table />
     </>
   );
 }
@@ -82,6 +84,7 @@ function TheLongWay ()
   );
 }
 
+// Ternary operators example:
 function ValidIndicator ()
 {
   const isValid = true;
@@ -93,6 +96,8 @@ function ValidIndicator ()
   );
 }
 
+// 4. Create a component that renders 'Hello, {username} if the username is a string
+// if the username is not a string output 'Not Logged In..'
 function Greeting ()
 {
   // Try all of these variations
@@ -104,8 +109,59 @@ function Greeting ()
   // Fill in the rest:
   return (
     <span>
-      {username ? 'Hello, ' + username : 'Not Logged In...'}
+      {username ? 'Hello, ' + username : 'Not Logged In...' }
+      <hr />
     </span>
+  );
+}
+
+/*
+ *5. One good way to learn a new syntax is to try breaking it – discover its boundaries. Try some of the things this chapter warned about and see what kind of errors you get.
+ * At the very least, it’ll familiarize you with what the errors mean if you make one of these mistakes later on.
+* a. Name a component starting with a lowercase letter, like “testComponent”.
+* b. Try returning 2 elements at once
+* c. Try returning an array with 2 elements inside
+* d. Can you put 2 expressions inside single braces, like {x && 5; x && 7}?
+* e. What happens if you use return inside a JS expression?
+* f. What about a function call like alert('hi')? Does it halt rendering?
+* g. Try putting a quoted string inside JSX. Does it strip out the quotes?
+ */
+
+/*
+  * 6. The HTML spec says that tables must be structured with a table element surrounding a tbody, which surrounds multiple trs( the rows ),
+  * which each surround multiple tds( the columns ).
+  * Create a component called Table that renders a table with 1 row and 3 columns and any data you like.
+  * Open the browser console and make sure there are no warnings.
+  */
+function Table ()
+{
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>First Name:</th>
+          <th>Last Name:</th>
+          <th>Age:</th>
+        </tr>
+        <tr>
+          <Data />
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+/*
+ * 6. Then, create a component called Data that renders the 3 columns, and replace the 3 < td > s with the < Data /> component.
+ */
+function Data ()
+{
+  return (
+    <>
+      <td>David</td>
+      <td>Gunner</td>
+      <td>36</td>
+    </>
   );
 }
 
